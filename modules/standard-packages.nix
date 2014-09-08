65fba4b09f448700a6960e0009bee0a02318a13c
Pavel Platto <hinidu@gmail.com>
2014-09-08 23:41:35 +0300

    Enable chromium plugins

--- modules/standard-packages.nix
+++ modules/standard-packages.nix
@@ -1,6 +1,16 @@
 { config, pkgs, ... }:
 
 {
+  nixpkgs.config = {
+    allowUnfree = true;
+
+    chromium = {
+      enableGoogleTalkPlugin = true;
+      enablePepperFlash = true;
+      enablePepperPdf = true;
+    };
+  };
+
   # List packages installed in system profile. To search by name, run:
   # -env -qaP | grep wget
   environment.systemPackages = with pkgs; [
