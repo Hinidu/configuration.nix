3d610785d94fad1eb064f52da897fcd28ef4bd09
Pavel Platto <hinidu@gmail.com>
2014-09-10 10:25:26 +0300

    Extract hinidu-notebook description

---
+++ computers/hinidu-notebook.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ ../modules/boot.nix
+      ../modules/i18n.nix
+      ../modules/networking.nix
+      ../modules/standard-packages.nix
+      ../modules/virtualbox-host.nix
+      ../modules/xserver.nix
+      ../users/hinidu.nix
+      ../users/olga.nix
+    ];
+
+  hardware.cpu.intel.updateMicrocode = true;
+
+  nix.gc.automatic = true;
+
+  time.timeZone = "Europe/Kaliningrad";
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  services.transmission.enable = true;
+}
