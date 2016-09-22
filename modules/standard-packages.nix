61fdb4c1a041d0103addf4860388a9796ee2d6f9
Pavel Platto <hinidu@gmail.com>
2016-09-22 22:36:14 +0200

    Clean up unused packages.

--- modules/standard-packages.nix
+++ modules/standard-packages.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    # androidenv.androidsdk_5_0_1
     aspell
     aspellDicts.en
     aspellDicts.ru
@@ -9,43 +8,22 @@
     ctags
     curl
     dmenu2
-    # erlang
-    # exercism
     file
     fish
-    # fsharp
     gcc
     gimp
     git
     gnumake
     (haskellPackages.ghcWithPackages (self: [
-      # self.cabal2nix
-      # self.clay
       self.ghc
-      # self.ghcMod
-      self.hakyll
-      # self.permutation
-      # self.testFrameworkHunit
       self.xmobar
       self.xmonad
     ]))
-    # idea.android-studio
-    # kibana
-    # logstash
-    mercurial
-    mono
-    # neovim
     nix-repl
-    # openjdk
     p7zip
     python
-    # sbt
-    # scala
-    # steam
     telnet
     testdisk
-    # tintin
-    # texLiveFull
     unrar
     unzip
     vagrant
@@ -56,15 +34,6 @@
     xorg.luit
     zip
   ];
-
-  # services.elasticsearch.enable = true;
-
-  # services.neo4j.enable = true;
-
-  services.postgresql = {
-    enable = true;
-    package = pkgs.postgresql94;
-  };
 
   services.teamviewer.enable = true;
 
