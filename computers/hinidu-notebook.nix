7ad3a4055e801cdd4df3b0bf7436381af1038625
Pavel Platto <hinidu@gmail.com>
2019-07-03 09:26:35 +0200

    Add hinidu_notebook configuration

---
+++ computers/hinidu-notebook.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ ../modules/env.nix
+      ../modules/i18n.nix
+      ../modules/misc.nix
+      ../modules/standard-packages.nix
+      ../modules/xserver.nix
+      ../users/hinidu.nix
+    ];
+
+  #fileSystems."/".options = ["defaults" "noatime" "discard" "inline_xattr"];
+  #fileSystems."/boot".options = ["defaults" "noatime" "discard"];
+
+  networking.hostName = "hinidu_notebook";
+  networking.firewall.enable = false;
+  networking.wireless.enable = true;
+  networking.wireless.networks = {
+    platto = {
+      pskRaw = "a221a823136bc15fa6c4fd919612c72113915fe9fa4582d6feda6d98bcf15814";
+    };
+  };
+
+  services.transmission.enable = true;
+  services.transmission.settings = {
+    download-dir = "/media/movies/";
+    incomplete-dir-enabled = false;
+    rpc-whitelist = "127.0.0.1,192.168.1.*";
+    umask = 2;
+  };
+}
