ab3ec3aacff16aafd3458fab22344d1bee2f793e
Pavel Platto <hinidu@gmail.com>
2020-08-21 10:04:50 +0200

    Remove obsolete slim displayManager

--- modules/xserver.nix
+++ modules/xserver.nix
@@ -14,27 +14,6 @@
       twoFingerScroll = true;
     };
 
-    displayManager.slim = {
-      # enable = true;
-      theme = pkgs.stdenv.mkDerivation {
-        name = "nixos-solarized-theme";
-        src = pkgs.fetchurl {
-          url = "https://github.com/Hinidu/nixos-solarized-slim-theme/archive/1.2.tar.gz";
-          sha256 = "f8918f56e61d4b8f885a4dfbf1285aeac7d7e53a7458e32942a759fedfd95faf";
-        };
-        phases = [ "unpackPhase" "patchPhase" "installPhase" ];
-        patchPhase = let
-          headCount = lib.length config.services.xserver.xrandrHeads;
-          headFactor = if headCount == 0 then 1 else headCount;
-          centerLeft = 100 / (headFactor * 2);
-        in ''
-          sed -i \
-            -e 's/^\(\(input_panel\|msg\|session\)_x\s\+\)[0-9]\+%/\1${toString centerLeft}%/' \
-            slim.theme
-        '';
-        installPhase = ''cp -R . "$out"'';
-      };
-    };
     displayManager.sessionCommands = ''
       xrdb "${pkgs.writeText "xrdb.conf" ''
         URxvt.font: xft:Source Code Pro:size=10:autohint=true:antialias=true
