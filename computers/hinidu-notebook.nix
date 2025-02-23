6c64ccda44dde76a9efa802c8bd577a800a53846
Pavel Platto <hinidu@gmail.com>
2025-02-23 19:48:03 +0200

    Update wireless networks

--- computers/hinidu-notebook.nix
+++ computers/hinidu-notebook.nix
@@ -19,10 +19,6 @@
   networking.firewall.enable = false;
   networking.wireless.enable = true;
   networking.wireless.networks = {
-    platto_repeater = {
-      pskRaw = "94987dc4da6ebce67d3716eb10add3a3071731b68313865f4f2e1ec80ab4f560";
-    };
-
     WARNING = {
       pskRaw = "8bd116eda8ffea342d351cacba288e5d2e5486730f1813a6481775857cbc9ed0";
     };
@@ -31,11 +27,12 @@
       pskRaw = "1aa52b600a923da39673567df28c800e25c5f2f100941c306831753ef3f768a8";
     };
 
-    # platto = {
-    #   pskRaw = "a221a823136bc15fa6c4fd919612c72113915fe9fa4582d6feda6d98bcf15814";
-    # };
+    platto = {
+      pskRaw = "a221a823136bc15fa6c4fd919612c72113915fe9fa4582d6feda6d98bcf15814";
+    };
 
     "iPhone (Павел)" = {
+      priority = 10;
       pskRaw = "0f1396b8121cc9e410ce791480b2dad34f997382c34a9169cf5930d0d7ad90c6";
     };
   };
