184b040113aff2115e3e0ddd9044a7dd41256715
Pavel Platto <hinidu@gmail.com>
2025-02-23 19:51:36 +0200

    Enable MiniDLNA

--- modules/media-server.nix
+++ modules/media-server.nix
@@ -8,4 +8,16 @@
     rpc-whitelist = "127.0.0.1,10.39.119.*";
     umask = 2;
   };
+
+  services.minidlna.enable = true;
+  services.minidlna.openFirewall = true;
+  services.minidlna.settings = {
+    inotify = "yes";
+    media_dir = [
+      "V,/media/torrents/"
+    ];
+  };
+  users.users.minidlna = {
+    extraGroups = ["transmission"];
+  };
 }
