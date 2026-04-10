251810a4cfc5dd3362343175dc10e58f1b034f1a
Pavel Platto <hinidu@gmail.com>
2026-04-10 08:11:36 +0200

    Better management of Wi-Fi

--- computers/hinidu-notebook.nix
+++ computers/hinidu-notebook.nix
@@ -16,23 +16,22 @@
 
   networking.hostName = "hinidu_notebook";
   networking.firewall.enable = false;
-  networking.wireless.enable = true;
-  networking.wireless.networks = {
-    WARNING = {
-      pskRaw = "8bd116eda8ffea342d351cacba288e5d2e5486730f1813a6481775857cbc9ed0";
-    };
+  networking.wireless = {
+    enable = true;
 
-    PanzerDog = {
-      pskRaw = "1aa52b600a923da39673567df28c800e25c5f2f100941c306831753ef3f768a8";
-    };
+    userControlled.enable = true;
 
-    platto = {
-      pskRaw = "a221a823136bc15fa6c4fd919612c72113915fe9fa4582d6feda6d98bcf15814";
-    };
+    secretsFile = "/etc/nixos/wifi-secrets.env";
 
-    "iPhone (Павел)" = {
-      priority = 10;
-      pskRaw = "0f1396b8121cc9e410ce791480b2dad34f997382c34a9169cf5930d0d7ad90c6";
+    networks = {
+      platto = {
+        pskRaw = "ext:psk_home";
+      };
+
+      "iPhone (Pavel)" = {
+        priority = 10;
+        pskRaw = "ext:psk_iphone";
+      };
     };
   };
 
