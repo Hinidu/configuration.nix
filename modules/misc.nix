6e54b9932b71347b92eec9fa1643514045e97067
Pavel Platto <hinidu@gmail.com>
2026-07-09 19:55:37 +0200

    Install russian certificates

--- modules/misc.nix
+++ modules/misc.nix
@@ -20,4 +20,15 @@
       value = "65536";
     }
   ];
+
+  security.pki.certificates = [
+    (builtins.readFile (pkgs.fetchurl {
+      url = "https://gu-st.ru/content/lending/russian_trusted_root_ca_pem.crt";
+      sha256 = "sha256-k2pD/qbo5SW8wPgazZw9IbT8S5torOp5BtaYAFr8ZQQ="; # Укажите актуальный хэш
+    }))
+    (builtins.readFile (pkgs.fetchurl {
+      url = "https://gu-st.ru/content/lending/russian_trusted_sub_ca_pem.crt";
+      sha256 = "sha256-8K5YnzZ3TynvNkj3mEsI1C/M5vH/7rYjbXc9rrJ0TqY="; # Укажите актуальный хэш
+    }))
+  ];
 }
